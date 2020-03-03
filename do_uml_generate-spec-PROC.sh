7c9e95c26745b7ac7dbd035d2ef0bc47a16c6798
wolandscat <thomas.beale@openehr.org>
2020-03-03 11:27:59 +0000

    Regeneration of diagrams

--- do_uml_generate-spec-PROC.sh
+++ do_uml_generate-spec-PROC.sh
@@ -1,4 +1,4 @@
 rm -f docs/UML/classes/*.*
 rm -f docs/UML/diagrams/*.*
 
-../specifications-AA_GLOBAL/bin/uml_generate.sh -i {proc_release} -r PROC -o docs/UML computable/UML/openEHR_UML-PROC.mdzip
+../specifications-AA_GLOBAL/bin/uml_generate.sh -d svg -i {proc_release} -r PROC -o docs/UML computable/UML/openEHR_UML-PROC.mdzip
