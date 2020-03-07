4fc319b49a755e76e5f3bc5938dc8c3cf008adfd
wolandscat <thomas.beale@openehr.org>
2020-03-07 18:36:52 +0000

    UML cleanup and remove regenerate script.

--- do_uml_generate-spec-PROC.sh
+++
@@ -1,4 +0,0 @@
-rm -f docs/UML/classes/*.*
-rm -f docs/UML/diagrams/*.*
-
-../specifications-AA_GLOBAL/bin/uml_generate.sh -d svg -i {proc_release} -r PROC -o docs/UML computable/UML/openEHR_UML-PROC.mdzip
